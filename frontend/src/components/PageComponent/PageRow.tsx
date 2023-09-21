7730653652c6fe741007ce0bce9f983d8da4eceb
onmetafox <onemetafox@outlook.com>
2023-09-22 07:16:31 +1000

    frontend: remove trans tag

--- frontend/src/components/PageComponent/PageRow.tsx
+++ frontend/src/components/PageComponent/PageRow.tsx
@@ -1,5 +1,4 @@
 import cx from "classnames";
-import { Trans, t } from "@lingui/macro";
 import "./PageRow.css";
 export default function PageRow(props) {
   const { title, value, direction, className, subValue} = props;
@@ -7,25 +6,25 @@ export default function PageRow(props) {
   return (
     <div className={classNames}>
       {(()=> {
-        if(direction == "align-right"){
+        if(direction === "align-right"){
           return (
             <>
-              <div className="col-6 title"><Trans>{title}</Trans></div>
+              <div className="col-6 title"><div>{title}</div></div>
               <div className="col-6 value align-right">{value} <span>{subValue}</span></div>
             </>
           )
-        }else if(direction == "vertical"){
+        }else if(direction === "vertical"){
           return (
             <>
-              <div className="title"><Trans>{title}</Trans></div>
-              <div className="value"><Trans>{value} <span>{subValue}</span></Trans></div>
+              <div className="title"><div>{title}</div></div>
+              <div className="value"><div>{value} <span>{subValue}</span></div></div>
             </>
           )
         }else{
           return (
             <>
-              <div className="col-6 title"><Trans>{title}</Trans></div>
-              <div className="col-6 value"><Trans>{value} <span>{subValue}</span></Trans></div>
+              <div className="col-6 title"><div>{title}</div></div>
+              <div className="col-6 value"><div>{value} <span>{subValue}</span></div></div>
             </>
           )
         }
