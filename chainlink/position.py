a208f049250731eff164081d564a5129b5a78fe3
onmetafox <onemetafox@outlook.com>
2024-01-23 05:46:53 +0900

    Update position.py

--- chainlink/position.py
+++ chainlink/position.py
@@ -11,7 +11,7 @@ import random
 config = {
     'endpoint': "https://goerli.base.org",
     'sleepTime': 10,
-    'keeper': "3b4b8d98a95a38a00817c95eb2d9e1bb826a544fe4d3eabe4276c877a0a74681",
+    'keeper': "",
     'positionRouterAddress': "0xC2493FC7B823cF32AdC6e316ea27136ad2718f4a",
 }
 
